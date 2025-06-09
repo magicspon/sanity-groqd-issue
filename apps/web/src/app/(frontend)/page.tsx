52b5e0332096fc108cf562d3c9654016b089fda8
David Stockley <magicspon@gmail.com>
2025-06-09 22:49:54 +0100

    refactor: page query fragment

--- apps/web/src/app/(frontend)/page.tsx
+++ apps/web/src/app/(frontend)/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import React from 'react'
+import { groupFragment } from '@spon/cms/src/fragments/group'
 import { q, z } from '@spon/cms/src/lib/groqd-client'
 import { getFirstOrNull } from '@spon/utils/getFirstOrNull'
 import { createPage } from '~/utils/createPage'
@@ -8,29 +9,19 @@ import { createSanityFetcher } from '~/utils/createSanityFetcher'
 const { Page, generateMetadata } = createPage({
 	loader: async () => {
 		const { fetcher } = await createSanityFetcher()
+		const query = q.star
+			.filterByType('page')
+			.slice(0, 1)
+			.project((sub) => ({
+				title: z.string(),
+				slug: ['slug.current', z.string()],
+				group: sub.field('group[]').project(groupFragment).nullable(true),
+			}))
+
+		console.log(query)
 
 		const response = await fetcher(
-			q.star
-				.filterByType('page')
-				.slice(0, 1)
-				.project((sub) => ({
-					title: z.string(),
-					slug: ['slug.current', z.string()],
-					group: sub.field('group[]').project((q1) => ({
-						...q1.conditionalByType({
-							group: q1.project({
-								_type: z.literal('group'),
-								_key: z.string(),
-							}),
-							letters: q1.project({
-								_type: z.literal('letters'),
-								_key: z.string(),
-								label: z.string(),
-								value: z.string(),
-							}),
-						}),
-					})),
-				})),
+			query,
 
 			{ next: { tags: ['home'] } },
 		)
@@ -51,6 +42,21 @@ const { Page, generateMetadata } = createPage({
 	},
 
 	component: async ({ data }) => {
+		console.log({ data })
+
+		if (data.group) {
+			const a = data.group.find((d) => d._type === 'group')
+			if (a && a._type === 'group') {
+				const b = a.group?.find((d) => d._type === 'group')
+				if (b && b._type === 'group') {
+					const c = b.group?.find((d) => d._type === 'group')
+					if (c && c._type === 'group') {
+						const d = c.group?.find((d) => d._type === 'group')
+					}
+				}
+			}
+		}
+
 		return <pre>{JSON.stringify(data, null, 2)}</pre>
 	},
 })
--- apps/web/src/app/(frontend)/provider.tsx
+++ apps/web/src/app/(frontend)/provider.tsx
@@ -1,23 +1,17 @@
 'use client'
 
 import { QueryClientProvider } from '@tanstack/react-query'
-import { LazyMotion, MotionConfig } from 'framer-motion'
+// import { LazyMotion, MotionConfig } from 'framer-motion'
 import type * as React from 'react'
 import { getQueryClient } from '~/utils/getQueryClient'
 
-const loadFeatures = () =>
-	import('~/utils/motionFeatures').then((res) => res.domAnimation)
+// const loadFeatures = () =>
+// 	import('~/utils/motionFeatures').then((res) => res.domAnimation)
 
 export function Provider({ children }: { children: React.ReactNode }) {
 	const queryClient = getQueryClient()
 
 	return (
-		<MotionConfig reducedMotion="user">
-			<LazyMotion features={loadFeatures}>
-				<QueryClientProvider client={queryClient}>
-					{children}
-				</QueryClientProvider>
-			</LazyMotion>
-		</MotionConfig>
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 	)
 }
